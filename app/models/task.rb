d5cee6493c7309031abcd0298efedc2e1a05e1ab
川口美咲 <m.kawaguchi1122@gmail.com>
2021-03-30 04:52:39 +0900

    [Add]名称にカンマを含めることができないように制限する

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
