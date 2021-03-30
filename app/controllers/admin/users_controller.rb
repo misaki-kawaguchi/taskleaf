5c64afcade9a645d4a617a94432514d3dca2874f
川口美咲 <m.kawaguchi1122@gmail.com>
2021-03-30 21:30:13 +0900

    [Add]ユーザー登録ページ作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,19 @@
 class Admin::UsersController < ApplicationController
+
+  # ユーザー登録フォーム
   def new
+    @user = User.new
+  end
+
+  # 登録アクション
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -9,5 +23,11 @@ class Admin::UsersController < ApplicationController
   end
 
   def index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end
