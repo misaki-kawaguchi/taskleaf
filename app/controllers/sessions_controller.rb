a81c55eb28f8ae0adb5998940f580019fcd7d9bf
川口美咲 <m.kawaguchi1122@gmail.com>
2021-03-31 21:28:59 +0900

    [Add]ログイン機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # 送られてきたメールアドレスでユーザー検索する
+    user = User.find_by(email: session_params[:email])
+
+    # 引数で受け取ったパスワードをハッシュ化して、その結果がUserオブジェクト内部に保存されているdigestと一致するか調べる
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'ログインしました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
