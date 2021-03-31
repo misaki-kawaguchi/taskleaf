2eda37583e2523c976858767ec9ad8ff16bfd472
川口美咲 <m.kawaguchi1122@gmail.com>
2021-03-31 22:40:54 +0900

    [Modify]ログインしているユーザのみタスクを登録できるようにする

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,12 @@ class TasksController < ApplicationController
 
   # 一覧表示
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   # 詳細表示
   def show
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   # 新規登録画面
@@ -17,7 +17,7 @@ class TasksController < ApplicationController
 
   # 登録アクション
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
     if @task.save
       redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
     else
@@ -27,19 +27,19 @@ class TasksController < ApplicationController
 
   # 編集画面
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   # データベースを更新
   def update
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   # 削除
   def destroy
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
