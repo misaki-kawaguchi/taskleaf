106eda21335a0d9904da9414b15ac6b314df7129
川口美咲 <m.kawaguchi1122@gmail.com>
2021-03-31 22:47:49 +0900

    [Add]管理者ユーザーだけに管理機能を利用できるようにする

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < ApplicationController
+  before_action :require_admin
 
   # ユーザー一覧
   def index
@@ -52,5 +53,9 @@ class Admin::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def require_admin
+    redirect_to root_url unique current_user.admin?
   end
 end
